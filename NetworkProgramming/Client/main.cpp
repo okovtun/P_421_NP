50b0f9b8cf6839b5a6887f59e9971740ba34606d
User <User@DESKTOP-QHG18FL>
2026-07-18 16:11:57 +0300

    FormatLastError almost DONE

--- NetworkProgramming/Client/main.cpp
+++ NetworkProgramming/Client/main.cpp
@@ -21,11 +21,32 @@ using std::endl;
 #define MTU		1500		//Maximum Transfer Unit - максимальный блок данных, который можно передать по сети.
 							//Для сетей семейства Ethernet MTU составляет 1500 Byte.
 
+CHAR* FormatLastError(DWORD dwError, CHAR szError[])
+{
+	ZeroMemory(szError, strlen(szError));
+	LPSTR lpError = NULL;
+	FormatMessage
+	(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+		NULL,
+		dwError,
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_NEUTRAL),
+		(LPSTR)&lpError,
+		256,
+		NULL
+	);
+	//strcpy(szError, lpError);
+	sprintf(szError, "Error# %i: %s ", dwError, lpError);
+	return szError;
+}
+
 void main()
 {
 	setlocale(LC_ALL, "");
 
 	INT iResult;	//эта переменная будет хранить результаты работы функций
+	DWORD dwError;
+	CHAR szError[256] = {};
 
 	//0) Инициализация WinSOCK:
 	WSADATA wsaData;
@@ -55,8 +76,10 @@ void main()
 
 	//2) Создаем сокет, при помощи которого будем подлючаться к Серверу:
 	SOCKET connect_socket = socket(target->ai_family, target->ai_socktype, target->ai_protocol);
+	dwError = WSAGetLastError();
 	if (connect_socket == INVALID_SOCKET)
 	{
+		cout << FormatLastError(dwError, szError) << endl;
 		cout << "Socket creation failed with error " << WSAGetLastError() << endl;
 		freeaddrinfo(target);
 		WSACleanup();
@@ -65,9 +88,11 @@ void main()
 
 	//3) Подключаемся к Серверу:
 	iResult = connect(connect_socket, target->ai_addr, target->ai_addrlen);
-	if (connect_socket == INVALID_SOCKET)
+	dwError = WSAGetLastError();
+	if (iResult)
 	{
 		cout << "Unable to connect to Server. Error: " << WSAGetLastError() << endl;
+		cout << FormatLastError(dwError, szError) << endl;
 		closesocket(connect_socket);
 		freeaddrinfo(target);
 		WSACleanup();
@@ -79,9 +104,11 @@ void main()
 	//4) Отправка данных на Сервер:
 	CHAR send_buffer[MTU] = "Привет Сервер!";
 	iResult = send(connect_socket, send_buffer, strlen(send_buffer), 0);
+	dwError = WSAGetLastError();
 	if (iResult == SOCKET_ERROR)
 	{
 		cout << "Send failed with error: " << WSAGetLastError() << endl;
+		cout << FormatLastError(dwError, szError) << endl;
 		closesocket(connect_socket);
 		WSACleanup();
 		return;
@@ -91,13 +118,19 @@ void main()
 	//5) Получение данных от Сервера:
 	CHAR recv_buffer[MTU] = {};
 	iResult = recv(connect_socket, recv_buffer, MTU, NULL);
+	dwError = WSAGetLastError();
 	if (iResult > 0)cout << iResult << "Byte received. Message: " << recv_buffer << endl;
 	else if (iResult == 0)cout << "Nothing received." << endl;
-	else cout << "Receive failed with error: " << WSAGetLastError() << endl;
+	else cout 
+		<< "Receive failed with error: " << WSAGetLastError() << endl 
+		<< FormatLastError(dwError, szError);
 
 	//6) Завершаем сеанс работы с Сервером и освобождаем ресурсы:
 	iResult = shutdown(connect_socket, SD_BOTH);	//закрываем соединение с Сервером в обоих направлениях
-	if (iResult == SOCKET_ERROR)cout << "Shutdown failed with error: " << WSAGetLastError() << endl;
+	dwError = WSAGetLastError();
+	if (iResult == SOCKET_ERROR)cout 
+		<< "Shutdown failed with error: " << WSAGetLastError() << endl
+		<< FormatLastError(dwError, szError);
 	closesocket(connect_socket);
 	WSACleanup();
 }
